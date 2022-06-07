553e7b7be7efe4e2582098f444b3be11dedc1bd2
PrasadDSandaruwan <prasaddarshana1999@gmail.com>
2022-06-07 19:28:50 +0530

    finalized

--- src/app/components/forcePasswordChange.jsx
+++ src/app/components/forcePasswordChange.jsx
@@ -1,97 +1,108 @@
-import React from "react";
-import Joi from "joi";
-import Form from "./common/form";
-import UserService from "../../services/user/userService";
-import { toast } from "react-toastify";
-import { Redirect } from "react-router-dom";
-import Auth from "../../services/user/authService";
+import React from 'react'
+import Joi from 'joi'
+import Form from './common/form'
+import UserService from '../../services/user/userService'
+import { toast } from 'react-toastify'
+import { Redirect } from 'react-router-dom'
+import Auth from '../../services/user/authService'
 
 export class ForcePasswordChange extends Form {
   state = {
     data: {
-      new_password: "",
-      confirm_password: "",
+      new_password: '',
+      confirm_password: '',
     },
     errors: {},
     isRedirect: false,
-  };
+  }
 
   schema = Joi.object({
-    new_password: Joi.string().required(),
-    confirm_password: Joi.string().required(),
-  });
+    new_password: Joi.string()
+      .required()
+      .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
+      .message('Passowrd is not strong enough'),
+    confirm_password: Joi.string()
+      .required()
+      .custom((value, helpers) => {
+        if (value !== this.state.data.new_password) {
+          return helpers.message("Password isn't match")
+        }
+        return value
+      })
+      .messages({ 'string.empty': 'Confirm password not allowed to be empty' }),
+  })
 
   doSubmit = async () => {
-    console.log("inside do submit");
+    console.log('inside do submit')
     try {
-      const response = await UserService.forceChangePassword(this.state.data);
-      console.log("response", response);
+      const response = await UserService.forceChangePassword(this.state.data)
+      console.log('response', response)
       if (response.status === 200) {
         if (response.data.code === 200) {
-          toast.success(response.data.message);
-          Auth.setForcePassword("VERIFIED_USER");
-          window.location = "/dasboard/rate-of-production";
+          toast.success(response.data.message)
+          Auth.setForcePassword('VERIFIED_USER')
+          window.location = '/dasboard/rate-of-production'
         } else {
-          toast.error(response.data.message);
+          toast.error(response.data.message)
         }
       } else {
-        toast.error(response.data.message);
+        toast.error(response.data.message)
       }
     } catch (ex) {
-      toast.error("Error Occured!");
+      toast.error('Error Occured!')
     }
-  };
+  }
 
   render() {
-    if (Auth.getForcePassword() === "VERIFIED_USER")
-      return <Redirect to="/dasboard/rate-of-production" />;
+    if (Auth.getForcePassword() === 'VERIFIED_USER')
+      return <Redirect to="/dasboard/rate-of-production" />
     return (
       <div>
         <div>
-          <div className="az-signin-wrapper " style={{ minHeight: "500px" }}>
+          <div className="az-signin-wrapper " style={{ minHeight: '500px' }}>
             <div
               className="az-card-signin"
-              style={{ justifyItems: "normal", height: "auto", width: "600px" }}
+              style={{ justifyItems: 'normal', height: 'auto', width: '600px' }}
             >
               <h4>Force Change Password</h4>
               <form>
                 <div className="form-group">
                   {this.renderInput(
-                    "new_password",
-                    "New Password",
-                    "Enter your password",
+                    'new_password',
+                    'New Password',
+                    'Enter your password',
                     null,
                     null,
-                    "password",
+                    'password',
                     null,
-                    null
+                    null,
                   )}
                 </div>
                 <div className="form-group">
                   {this.renderInput(
-                    "confirm_password",
-                    "Confirm Password",
-                    "Enter confirm password",
+                    'confirm_password',
+                    'Confirm Password',
+                    'Enter confirm password',
                     null,
                     null,
-                    "password",
+                    'password',
                     null,
-                    null
+                    null,
                   )}
                 </div>
                 {this.renderButton(
-                  "Change Password",
-                  "Change Password",
+                  'Change Password',
+                  'Change Password',
                   null,
-                  null
+                  null,
                 )}
               </form>
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default ForcePasswordChange;
+export default ForcePasswordChange
--- src/app/pages/ChangePassword.jsx
+++ src/app/pages/ChangePassword.jsx
@@ -1,103 +1,114 @@
-import React from "react";
-import Joi from "joi";
-import Form from "../components/common/form";
-import userService from "../../services/user/userService";
-import { toast } from "react-toastify";
+import React from 'react'
+import Joi from 'joi'
+import Form from '../components/common/form'
+import userService from '../../services/user/userService'
+import { toast } from 'react-toastify'
 
 export class ChangePassword extends Form {
   state = {
     data: {
-      new_password: "",
-      old_password: "",
-      confirm_password: "",
+      new_password: '',
+      old_password: '',
+      confirm_password: '',
     },
     errors: {},
-  };
+  }
 
   schema = Joi.object({
-    new_password: Joi.string().required(),
     old_password: Joi.string().required(),
-    confirm_password: Joi.string().required(),
-  });
+    new_password: Joi.string()
+      .required()
+      .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
+      .message('Passowrd is not strong enough'),
+    confirm_password: Joi.string()
+      .required()
+      .custom((value, helpers) => {
+        if (value !== this.state.data.new_password) {
+          return helpers.message("Password isn't match")
+        }
+        return value
+      })
+      .messages({ 'string.empty': 'Confirm password not allowed to be empty' }),
+  })
 
   doSubmit = async () => {
-    const { password } = this.state.data;
+    const { password } = this.state.data
     try {
-      const response = await userService.changePassword(this.state.data);
+      const response = await userService.changePassword(this.state.data)
       if (response.status === 200) {
         if (response.data.code === 200) {
-          toast.success(response.data.message);
+          toast.success(response.data.message)
         } else {
-          toast.error(response.data.message);
+          toast.error(response.data.message)
         }
       } else {
-        toast.error(response.data.message);
+        toast.error(response.data.message)
       }
     } catch (ex) {
-      toast.error("Error Occured!");
+      toast.error('Error Occured!')
     }
-  };
+  }
 
   render() {
     return (
       <div>
         <div>
-          <div className="az-signin-wrapper " style={{ minHeight: "500px" }}>
+          <div className="az-signin-wrapper " style={{ minHeight: '500px' }}>
             <div
               className="az-card-signin"
-              style={{ justifyItems: "normal", height: "auto", width: "600px" }}
+              style={{ justifyItems: 'normal', height: 'auto', width: '600px' }}
             >
               <h4>Change Password</h4>
               <form>
                 <div className="form-group">
                   {this.renderInput(
-                    "old_password",
-                    "Old Password",
-                    "Enter old password",
+                    'old_password',
+                    'Old Password',
+                    'Enter old password',
                     null,
                     null,
-                    "password",
+                    'password',
                     null,
-                    null
+                    null,
                   )}
                 </div>
                 <div className="form-group">
                   {this.renderInput(
-                    "new_password",
-                    "New Password",
-                    "Enter your new password",
+                    'new_password',
+                    'New Password',
+                    'Enter your new password',
                     null,
                     null,
-                    "password",
+                    'password',
                     null,
-                    null
+                    null,
                   )}
                 </div>
                 <div className="form-group">
                   {this.renderInput(
-                    "confirm_password",
-                    "Confirm Password",
-                    "Confirm your password",
+                    'confirm_password',
+                    'Confirm Password',
+                    'Confirm your password',
                     null,
                     null,
-                    "password",
+                    'password',
                     null,
-                    null
+                    null,
                   )}
                 </div>
                 {this.renderButton(
-                  "Change Password",
-                  "Change Password",
+                  'Change Password',
+                  'Change Password',
                   null,
-                  null
+                  null,
                 )}
               </form>
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default ChangePassword;
+export default ChangePassword
